5ed28f30cbea01f760c851acb3ab687f1954b066
Pavel Tsayukov <p.tsayukov@gmail.com>
2024-07-13 14:43:42 +0300

    Add test for 'is_iterable_v'

--- tests/flatten_iterator_test.cpp
+++ tests/flatten_iterator_test.cpp
@@ -3,4 +3,78 @@
 
 #include <gtest/gtest.h>
 
+#include <array>
+#include <deque>
+#include <forward_list>
+#include <iterator>
+#include <list>
+#include <vector>
+
+// Containers with different kinds of iterators
+
+template <typename T>
+using ContinuousC = std::vector<T>;
+
+template <typename T>
+using RandomAccessC = std::deque<T>;
+
+template <typename T>
+using BidirectC = std::list<T>;
+
+template <typename T>
+using ForwardC = std::forward_list<T>;
+
+template <typename T>
+using InputC = std::istream_iterator<T>;
+
+// Built-in array type
+template <typename T, std::size_t N>
+using Array = T[N];
+
+// For checking if argument-dependent lookup works properly
+namespace adl {
+
+struct ContainerWithFreeBeginEnd {
+    std::array<int, 2> array;
+};
+
+auto begin(ContainerWithFreeBeginEnd& c) noexcept {
+    return c.array.begin();
+}
+
+auto end(ContainerWithFreeBeginEnd& c) noexcept {
+    return c.array.end();
+}
+
+class ContainerWithFriendBeginEnd {
+    std::array<int, 2> array_;
+public:
+    friend auto begin(ContainerWithFriendBeginEnd& c) noexcept {
+        return c.array_.begin();
+    }
+
+    friend auto end(ContainerWithFriendBeginEnd& c) noexcept {
+        return c.array_.end();
+    }
+};
+
+} // namespace adl
+
+
+TEST(Traits, IsIterable) {
+    namespace fidt = flatten_iterator::details::traits;
+
+    static_assert(!fidt::is_iterable_v<int>, "`int` is not iterable");
+    static_assert(fidt::is_iterable_v<Array<int, 42>>,
+                  "Built-in array is iterable");
+    static_assert(fidt::is_iterable_v<ContinuousC<int>>,
+                  "Regular container is iterable");
+    static_assert(fidt::is_iterable_v<std::vector<bool>>,
+                  "`std::vector<bool>` is iterable");
+    static_assert(fidt::is_iterable_v<adl::ContainerWithFreeBeginEnd>,
+                  "Class with free `begin/end` is iterable");
+    static_assert(fidt::is_iterable_v<adl::ContainerWithFriendBeginEnd>,
+                  "Class with friend `begin/end` is iterable");
+}
+
 // TODO: add tests
